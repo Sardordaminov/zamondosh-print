47922f6feab11864ba959b919e2ffd5c73b07629
Lesbi <Gey@gmail.com>
2023-06-01 18:23:29 +0500

    salom1

--- script.js
+++ script.js
@@ -16,12 +16,3 @@ opener.addEventListener("click", () => {
         navbar.classList.add("full")
     }
 })
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
