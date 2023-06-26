19925d1bbc9e77d6774b81213f48c2ac117a9a80
Sardor Daminov <sardorbe221007@gmail.com>
2023-06-26 13:10:58 +0500

    multilang completed

--- translate.js
+++ translate.js
@@ -1,33 +1,43 @@
 let langs = document.querySelector(".translator"),
     link = document.querySelectorAll(".link"),
     li1 = document.querySelector(".child1")
-    li2 = document.querySelector(".child2")
-    li3 = document.querySelector(".child3")
-    li4 = document.querySelector(".child4")
-    li5 = document.querySelector(".child5")
-    li6 = document.querySelector(".child6")
-    title = document.querySelector(".header-main h1"),
+li2 = document.querySelector(".child2")
+li3 = document.querySelector(".child3")
+li4 = document.querySelector(".child4")
+li5 = document.querySelector(".child5")
+li6 = document.querySelector(".child6")
+title = document.querySelector(".header-main h1"),
     descr = document.querySelector(".header-main p"),
     header_btn1 = document.querySelector(".btn.gray"),
     header_btn2 = document.querySelector(".btn.second")
-    about_us_title = document.querySelector(".text h2"),
+about_us_title = document.querySelector(".text h2"),
     about_us_descr = document.querySelector(".text p"),
     about_us_btn = document.querySelector(".hire"),
     our_services_title = document.querySelector(".our-services-title"),
     services_cart_title1 = document.querySelector('.services_cart_title1'),
-    services_cart_title2 = document.querySelector('.services_cart_title2'),    
-    services_cart_title3 = document.querySelector('.services_cart_title3'),    
-    services_cart_title4 = document.querySelector('.services_cart_title4'),    
-    services_cart_title5 = document.querySelector('.services_cart_title5'),    
+    services_cart_title2 = document.querySelector('.services_cart_title2'),
+    services_cart_title3 = document.querySelector('.services_cart_title3'),
+    services_cart_title4 = document.querySelector('.services_cart_title4'),
+    services_cart_title5 = document.querySelector('.services_cart_title5'),
     services_cart_title6 = document.querySelector('.services_cart_title6'),
-    services_cart_descr1 = document.querySelector(".services_cart_descr1"),   
-    services_cart_descr2 = document.querySelector(".services_cart_descr2"),   
-    services_cart_descr3 = document.querySelector(".services_cart_descr3"),   
-    services_cart_descr4 = document.querySelector(".services_cart_descr4"),   
-    services_cart_descr5 = document.querySelector(".services_cart_descr5"),   
+    services_cart_descr1 = document.querySelector(".services_cart_descr1"),
+    services_cart_descr2 = document.querySelector(".services_cart_descr2"),
+    services_cart_descr3 = document.querySelector(".services_cart_descr3"),
+    services_cart_descr4 = document.querySelector(".services_cart_descr4"),
+    services_cart_descr5 = document.querySelector(".services_cart_descr5"),
     services_cart_descr6 = document.querySelector(".services_cart_descr6"),
     products_title = document.querySelector(".products-title"),
     show_products = document.querySelector(".show-products p"),
+
+    slider_title = document.querySelector(".slider h1"),
+    faq_title = document.querySelector(".faq_textbox h1"),
+    faq_descr = document.querySelector(".faq_textbox h3"),
+    faq_p = document.querySelector(".faq_textbox_p"),
+    view_more = document.querySelector(".view-more p"),
+    footer_div_a = document.querySelector(".footer_div.a p"),
+    footer_div_b = document.querySelector(".footer_div.b .div_title"),
+    footer_div_last = document.querySelector(".footer_div.last .div_title"),
+    copyright_p = document.querySelector(".copy-right-sec p");
 
 link.forEach(el => {
     el.addEventListener("click", () => {
@@ -62,6 +72,16 @@ link.forEach(el => {
         services_cart_descr6.textContent = data[attr].services_cart_descr6
         products_title.textContent = data[attr].products_title
         show_products.textContent = data[attr].show_products
+
+        slider_title.textContent = data[attr].slider_title
+        faq_title.textContent = data[attr].faq_title
+        faq_descr.textContent = data[attr].faq_descr
+        faq_p.textContent = data[attr].faq_p
+        view_more.textContent = data[attr].view_more
+        footer_div_a.textContent = data[attr].footer_div_a
+        footer_div_b.textContent = data[attr].footer_div_b
+        footer_div_last.textContent = data[attr].footer_div_last
+        copyright_p.textContent = data[attr].copyright_p
     })
 })
 
@@ -95,6 +115,16 @@ let data = {
         services_cart_descr6: "All the colorful prints you want with modern design",
         products_title: "PRODUCTS",
         show_products: `Show Products`,
+
+        slider_title: `Our Partners`,
+        faq_title: `Frequently Asked Questions`,
+        faq_descr: `What is printing services?`,
+        faq_p: `Alpha Color Store offers a wide variety of printing and finishing services, including electronic file access (e.g., e-mails, CDs, USB drives)`,
+        view_more: `VIEW MORE`,
+        footer_div_a: `We specialize in commercial offset printing and digital printing services to cover.`,
+        footer_div_b: `Contact Info`,
+        footer_div_last: `Opening Hours`,
+        copyright_p: `2023 Zamondosh. All rights reserved`,
     },
     russian: {
         li1: "Главная",
@@ -125,5 +155,15 @@ let data = {
         services_cart_descr6: "Любые красочные принты с современным дизайном",
         products_title: "ПРОДУКТЫ",
         show_products: "Показать продукты",
+
+        slider_title: `Наши партнеры`,
+        faq_title: `Часто задаваемые вопросы`,
+        faq_descr: `Что такое полиграфические услуги?`,
+        faq_p: `Alpha Color Store предлагает широкий спектр услуг печати и финишной обработки, включая доступ к электронным файлам (например, сообщениям электронной почты, компакт-дискам, USB-накопителям).`,
+        view_more: `ПОСМОТРЕТЬ БОЛЬШЕ`,
+        footer_div_a: `Мы специализируемся на коммерческой офсетной печати и услугах цифровой печати.`,
+        footer_div_b: `Контактная информация`,
+        footer_div_last: `Часы работы`,
+        copyright_p: `2023 Zamondosh. Все права защищены`,
     }
 }
